df09744bc908acd15a357273ed9d03344a89c58a
John Hancock <john@dev.jhnhnck.com>
2018-05-23 21:48:54 -0400

    Verbose options and getter/setters

--- src/Ironsmith.ts
+++ src/Ironsmith.ts
@@ -3,193 +3,268 @@
  * Author(s): John Hancock <john@dev.jhnhnck.com>
  */
 
-import * as fs from 'fs-extra'
-import { clone, merge } from 'lodash'
-import * as path from 'path'
-import * as recursive from 'recursive-readdir'
+	import * as debug from 'debug'
+	import * as fs from 'fs-extra'
+	import { clone, merge } from 'lodash'
+	import * as path from 'path'
+	import * as recursive from 'recursive-readdir'
 
-import { File } from './File'
-export declare namespace Ironsmith {
-  type FileMap = Map<string, Ironsmith.File>
-  type Next = () => void
-  type Plugin = (files: Ironsmith.FileMap, fe: Ironsmith, next: Ironsmith.Next) => void
-  interface Metadata { [index: string]: any }
+	import { File } from './File'
 
-  namespace File {
-    type Augment = (file: File) => any
-    type Tags = Set<string>
+	const log = debug('ironsmith')
 
-    interface Options {
-      tags?: string[] | File.Tags
-      asset?: boolean
-      [index: string]: any
-    }
-  }
-
-  interface File {
-    asset: boolean
-    contents: Buffer | string | any
-    path: string
-    tags: File.Tags
-    [index: string]: any
-  }
-
-  class File {
-    private static _augment
-
-    constructor(contents: Buffer | string | any, path: string, properties?: File.Options)
-    private initialize()
-
-    public static addAugment(ftn: File.Augment): void
-  }
-}
-
-export class Ironsmith {
-  private plugins: Ironsmith.Plugin[] = []
-  private files: Ironsmith.FileMap
-
-  private _rootPath: string = __dirname
-  private _sourcePath: string = './src'
-  private _buildPath: string = './build'
-  private _assetsPath: string = './assets'
-
-  private _metadata: Ironsmith.Metadata = {}
-  private _clean: boolean = true
-  private _loadAssets: boolean = true
+	export declare namespace Ironsmith {
+	  type FileMap = Map<string, Ironsmith.File>
+	  type Next = () => void
+	  type Plugin = (files: Ironsmith.FileMap, fe: Ironsmith, next: Ironsmith.Next) => void
+	  interface Metadata { [index: string]: any }
 
 
-  /* Initialize a new `Ironsmith` builder */
-  constructor(directory?: string) {
-    this.setRoot(directory || this._rootPath)
-    this.setSourcePath(this._sourcePath)
-    this.setBuildPath(this._buildPath)
-    this.setAssetsPath(this._assetsPath)
-  }
+	  interface Options {
+	    rootPath?: string
+	    sourcePath?: string
+	    buildPath?: string
+	    assetsPath?: string
+	    clean?: boolean
+	    loadAssets?: boolean
+	    metadata?: Metadata
+	    verbose?: boolean
+	  }
 
-  /* Get the current working directory */
-  public root(): string { return this._rootPath }
 
-  /* Get the directory of the output files */
-  public buildPath(): string { return this._buildPath }
+	  namespace File {
+	    type Augment = (file: File) => any
+	    type Tags = Set<string>
 
-  /* Get the source directory */
-  public sourcePath(): string { return this._sourcePath }
 
-  /* Returns either the assets path or false if the feature is disabled */
-  public assetsPath(): string | boolean { return this._loadAssets ? this._assetsPath : false }
+	    interface Options {
+	      tags?: string[] | File.Tags
+	      asset?: boolean
+	      [index: string]: any
+	    }
+	  }
 
-  /* Enable or disable the loading of static assets */
-  public loadAssets(state: boolean): Ironsmith { this._loadAssets = state; return this }
 
-  /* Get whether the destination directory will be removed before writing. */
-  public isClean(): boolean { return this._clean }
+	  interface File {
+	    asset: boolean
+	    contents: Buffer | string | any
+	    path: string
+	    tags: File.Tags
+	    [index: string]: any
+	  }
 
-  /* Set whether the destination directory will be removed before writing. */
-  public setClean(state: boolean): Ironsmith { this._clean = state; return this }
 
-  /* Returns the currently stored metadata object */
-  public metadata(): Ironsmith.Metadata { return this._metadata }
+	  class File {
+	    private static _augment
 
-  /* Sets the current metadata to match the object passed */
-  public setMetadata(metadata: Ironsmith.Metadata): Ironsmith { this._metadata = clone(metadata); return this }
 
-  /* Merges the current metadata with the object passed */
-  public mergeMetadata(metadata: Ironsmith.Metadata): Ironsmith { merge(this._metadata, metadata); return this }
+	    constructor(contents: Buffer | string | any, path: string, properties?: File.Options)
+	    private initialize()
 
-  /* Set the current working directory */
-  public setRoot(directory: string): Ironsmith {
-    if (directory.length > 0) {  // TODO: Check if this is a valid path
-      this._rootPath = path.resolve(directory)
-    } else {
-      console.warn('Warning! Blatantly refusing to make the root path blank.')
-    }
 
-    return this
-  }
+	    public static addAugment(ftn: File.Augment): void
+	  }
+	}
 
-  /* Set the directory of the output files */
-  public setBuildPath(directory: string): Ironsmith {
-    if (directory.length > 0) {  // TODO: Check if this is a valid path
-      this._buildPath = path.resolve(this._rootPath, directory)
-    } else {
-      console.warn('Warning! Blatantly refusing to make the build path blank.')
-    }
 
-    return this
-  }
+	export class Ironsmith {
+	  private plugins: Ironsmith.Plugin[] = []
+	  private files: Ironsmith.FileMap
 
-  /* Set the source directory */
-  public setSourcePath(directory: string): Ironsmith {
-    if (directory.length > 0) {  // TODO: Check if this is a valid path
-      this._sourcePath = path.resolve(this._rootPath, directory)
-    } else {
-      console.warn('Warning! Blatantly refusing to make the source path blank.')
-    }
 
-    return this
-  }
+	  private _rootPath: string = __dirname
+	  private _sourcePath: string = './src'
+	  private _buildPath: string = './build'
+	  private _assetsPath: string = './assets'
 
-  /* Specify an alternative directory for loading additional static files */
-  public setAssetsPath(directory: string): Ironsmith {
-    if (directory.length > 0) {  // TODO: Check if this is a valid path
-      this._assetsPath = path.resolve(this._rootPath, directory)
-    } else {
-      this._loadAssets = false
-    }
 
-    return this
-  }
+	  private _metadata: Ironsmith.Metadata = {}
+	  private _clean: boolean = true
+	  private _loadAssets: boolean = true
 
-  /* Add a 'Ironsmith.Plugin' function to the stack */
-  public use(plugin: Ironsmith.Plugin): Ironsmith { this.plugins.push(plugin); return this }
 
-  /* Build with the current settings to the destination directory. */
-  public async build(): Promise<Ironsmith.FileMap> {
-    if (this._clean) { await fs.remove(path.join(this._buildPath, '*')) }
 
-    await this.process()
-    await Ironsmith.dumpDirectory(this.files, this._buildPath)
-    return this.files
-  }
 
-  /* Process files through plugins without writing the output files. */
-  public async process(): Promise<Ironsmith.FileMap> {
-    const sources = await Ironsmith.loadDirectory(this._sourcePath, {asset: false})
-    const assets = await Ironsmith.loadDirectory(this._assetsPath, {asset: true})
-    this.files = new Map([...sources, ...assets])
+	  /* Initialize a new `Ironsmith` builder */
+	  constructor(options?: Ironsmith.Options) {
+	    if (options !== undefined && options.verbose !== undefined) {
+	      log.enabled = options.verbose
+	    }
 
-    for (const plugin of this.plugins) {
-      await new Promise((next) => { plugin(this.files, this, next) })
-    }
 
-    return this.files
-  }
+	    Object.assign(this, options)
+	  }
 
-  /* --- Static FileMap Functions --- */
 
-  /* Abstracts the reading of files so it can be used more abstractly */
-  public static async loadDirectory(directory: string, properties?: Ironsmith.File.Options): Promise<Ironsmith.FileMap> {
-    const filenames = await recursive(path.resolve(directory))
-    const files: Ironsmith.FileMap = new Map()
+	  /* --- Working Directory --- */
 
-    for (const name of filenames) {
-      const buffer = await fs.readFile(name)
 
-      const file = new Ironsmith.File(buffer, path.relative(directory, name), properties)
+	  get rootPath(): string { return this._rootPath }
 
-      files.set(file.path, file)
-    }
 
-    return files
-  }
+	  set rootPath(directory: string) {
+	    if (directory.length > 0) {  // TODO: Check if this is a valid path
+	      this._rootPath = path.resolve(directory)
+	      log(`setRoot(...): ${this._rootPath}`)
+	    } else {
+	      console.warn('Warning! Blatantly refusing to make the root path blank.')
+	    }
+	  }
 
-  /* Writes a `Ironsmith.FileMap` into directory */
-  public static async dumpDirectory(files: Ironsmith.FileMap, directory: string): Promise<void> {
-    files.forEach(async (file, name) => {
-      const absPath = path.resolve(directory, name)
 
-      await fs.mkdirs(path.dirname(absPath))
-      await fs.writeFile(absPath, file.contents)
-    })
-  }
-}
+	  /* --- Output Directory --- */
+
+
+	  get buildPath(): string { return this._buildPath }
+
+
+	  set buildPath(directory: string) {
+	    if (directory.length > 0) {  // TODO: Check if this is a valid path
+	      this._buildPath = path.resolve(this._rootPath, directory)
+	      log(`setBuildPath(...): ${this._buildPath}`)
+	    } else {
+	      console.warn('Warning! Blatantly refusing to make the build path blank.')
+	    }
+	  }
+
+
+	  /* --- Source Directory --- */
+
+
+	  get sourcePath(): string { return this._sourcePath }
+
+
+	  set sourcePath(directory: string) {
+	    if (directory.length > 0) {  // TODO: Check if this is a valid path
+	      this._sourcePath = path.resolve(this._rootPath, directory)
+	      log(`setSourcePath(...): ${this._sourcePath}`)
+	    } else {
+	      console.warn('Warning! Blatantly refusing to make the source path blank.')
+	    }
+	  }
+
+
+	  /* --- Assets Directory --- */
+
+
+	  get assetsPath(): string { return this._loadAssets ? this._assetsPath : undefined }
+
+
+	  set assetsPath(directory: string) {
+	    if (directory.length > 0) {  // TODO: Check if this is a valid path
+	      this._assetsPath = path.resolve(this._rootPath, directory)
+	      log(`setAssetsPath(...): ${this._assetsPath}`)
+	    } else {
+	      this._loadAssets = false
+	    }
+	  }
+
+
+	  /* --- Process Metadata --- */
+
+
+	  get metadata(): Ironsmith.Metadata { return this._metadata }
+	  set metadata(metadata: Ironsmith.Metadata) { this._metadata = metadata }
+	  public mergeMetadata(metadata: Ironsmith.Metadata) { merge(this._metadata, metadata) }
+
+
+	  /* --- Other Properties --- */
+
+
+	  get clean(): boolean { return this._clean }
+	  set verbose(value: boolean) { log.enabled = value }
+
+
+	  /* --- Build Process --- */
+
+
+	  /* Add a 'Ironsmith.Plugin' function to the stack */
+	  public use(plugin: Ironsmith.Plugin): Ironsmith {
+	    log(`use(...): Registered plugin "${plugin.name}"`)
+	    this.plugins.push(plugin)
+	    return this
+	  }
+
+
+	  /* Build with the current settings to the destination directory. */
+	  public async build(): Promise<Ironsmith.FileMap> {
+	    if (this._clean) {
+	      log(`build(): Cleaning build directory`)
+	      await fs.remove(path.join(this._buildPath, '*'))
+	    }
+
+
+	    await this.process()
+	    await Ironsmith.dumpDirectory(this.files, this._buildPath)
+	    return this.files
+	  }
+
+
+	  /* Process files through plugins without writing the output files. */
+	  public async process(): Promise<Ironsmith.FileMap> {
+	    const sources = await Ironsmith.loadDirectory(this._sourcePath)
+
+
+	    if (this._loadAssets) {
+	      const assets = await Ironsmith.loadDirectory(this._assetsPath, {asset: true})
+	      this.files = new Map([...sources, ...assets])
+	    } else {
+	      this.files = sources
+	    }
+
+
+	    for (const plugin of this.plugins) {
+	      await new Promise((next) => {
+	        console.log(); log(`process(): Running plugin ${plugin.name}`)
+	        plugin(this.files, this, next)
+	      })
+	    }
+
+
+	    return this.files
+	  }
+
+
+	  /* --- Static FileMap Functions --- */
+
+
+	  /* Abstracts the reading of files so it can be used more abstractly */
+	  public static async loadDirectory(directory: string, properties?: Ironsmith.File.Options): Promise<Ironsmith.FileMap> {
+	    const filenames = await recursive(path.resolve(directory))
+	    const files: Ironsmith.FileMap = new Map()
+
+
+	    log(`loadDirectory(...): Loading ${filenames.length} files from ${directory}`)
+
+
+	    for (const name of filenames) {
+	      const buffer = await fs.readFile(name)
+
+
+	      const file = new Ironsmith.File(buffer, path.relative(directory, name), properties)
+
+
+	      log(`Loading file: ${file.path}${file.asset ? ' (asset)' : '' }`)
+	      files.set(file.path, file)
+	    }
+
+
+	    return files
+	  }
+
+
+	  /* Writes a `Ironsmith.FileMap` into directory */
+	  public static async dumpDirectory(files: Ironsmith.FileMap, directory: string): Promise<void> {
+	    log(`dumpDirectory(...): Dumping ${files.size} files into ${directory}`)
+
+
+	    files.forEach(async (file, name) => {
+	      const absPath = path.resolve(directory, name)
+
+
+	      await fs.mkdirs(path.dirname(absPath))
+	      log(`Writing file: ./${name}${file.asset ? ' (asset)' : '' }`)
+	      await fs.writeFile(absPath, file.contents)
+	    })
+	  }
+	}
--- src/types.d.ts
+++ src/types.d.ts
@@ -11,6 +11,17 @@ export declare namespace Ironsmith {
   type Plugin = (files: Ironsmith.FileMap, fe: Ironsmith, next: Ironsmith.Next) => void
   interface Metadata { [index: string]: any }
 
+  interface Options {
+    rootPath?: string
+    sourcePath?: string
+    buildPath?: string
+    assetsPath?: string
+    clean?: boolean
+    loadAssets?: boolean
+    metadata?: Metadata
+    verbose?: boolean
+  }
+
   namespace File {
     type Augment = (file: File) => any
     type Tags = Set<string>
@@ -41,62 +52,36 @@ export declare namespace Ironsmith {
 }
 
 export declare class Ironsmith {
-  private plugins: Ironsmith.Plugin
-  private files: Ironsmith.FileMap
+  private plugins
+  private files
+  private _rootPath
+  private _sourcePath
+  private _buildPath
+  private _assetsPath
+  private _metadata
+  private _clean
+  private _loadAssets
 
-  private _rootPath: string
-  private _sourcePath: string
-  private _buildPath: string
-  private _assetsPath: string
+  constructor(options?: Ironsmith.Options)
 
-  private _metadata: Ironsmith.Metadata
-  private _clean: boolean
-  private _loadAssets: boolean
+  /* --- Directories --- */
 
-  /* Initialize a new `Ironsmith` builder */
-  constructor(directory?: string)
+  rootPath: string
+  buildPath: string
+  sourcePath: string
+  assetsPath: string
 
-  /* Get the current working directory */
-  root(): string
+  /* --- Process Metadata --- */
 
-  /* Set the current working directory */
-  setRoot(directory: string): Ironsmith
+  metadata: Ironsmith.Metadata
+  mergeMetadata(metadata: Ironsmith.Metadata): void
 
-  /* Get the source directory */
-  sourcePath(): string
+  /* --- Other Properties --- */
 
-  /* Set the source directory */
-  setSourcePath(directory: string): Ironsmith
+  readonly clean: boolean
+  verbose: boolean
 
-  /* Get the directory of the output files */
-  buildPath(): string
-
-  /* Set the directory of the output files */
-  setBuildPath(directory: string): Ironsmith
-
-  /* Specify an alternative directory for loading additional static files */
-  setAssetsPath(directory: string): Ironsmith
-
-  /* Returns either the assets path or false if the feature is disabled */
-  assets(): string | boolean
-
-  /* Enable or disable the loading of static assets */
-  loadAssets(state: boolean): Ironsmith
-
-  /* Set whether the destination directory will be removed before writing. */
-  isClean(): boolean
-
-  /* Set whether the destination directory will be removed before writing. */
-  setClean(state: boolean): Ironsmith
-
-  /* Returns the currently stored metadata object */
-  metadata(): Ironsmith.Metadata
-
-  /* Sets the current metadata to match the object passed */
-  setMetadata(metadata: Ironsmith.Metadata): Ironsmith
-
-  /* Merges the current metadata with the object passed */
-  mergeMetadata(metadata: Ironsmith.Metadata): Ironsmith
+  /* --- Build Process --- */
 
   /* Add a 'Ironsmith.Plugin' function to the stack */
   use(plugin: Ironsmith.Plugin): Ironsmith
@@ -107,9 +92,8 @@ export declare class Ironsmith {
   /* Process files through plugins without writing the output files. */
   process(): Promise<Ironsmith.FileMap>
 
-  /* Abstracts the reading of files so it can be used more abstractly */
-  static loadDirectory(directory: string, tags?: object): Promise<Ironsmith.FileMap>
+  /* --- Static FileMap Functions --- */
 
-  /* Writes a `Ironsmith.FileMap` into directory */
+  static loadDirectory(directory: string, properties?: Ironsmith.File.Options): Promise<Ironsmith.FileMap>
   static dumpDirectory(files: Ironsmith.FileMap, directory: string): Promise<void>
 }
