abec287df4478e001d2422ca9056a5ee991f8358
John Hancock <john@dev.jhnhnck.com>
2018-07-03 22:28:59 -0400

    Updated configs for publishing

--- ironsmith.js
+++ ironsmith.js
@@ -3,7 +3,5 @@
  * Author(s): John Hancock <john@dev.jhnhnck.com>
  */
 
-require('source-map-support').install()
-
 exports.Ironsmith = require('./bin/Ironsmith').Ironsmith
 exports.Ironsmith.File = require('./bin/File').IronsmithFile
